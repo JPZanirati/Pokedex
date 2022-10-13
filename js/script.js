14152b10eaed93295ae42e10b81c13ac2684159c
João Pedro Zanirati <nunesmln6498@gmail.com>
2022-10-13 19:16:06 -0300

    Finalizando Pokedex

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const pokemonName = document.querySelector(".pokemon__name");
+const pokemonNumber = document.querySelector(".pokemon__number");
+const pokemonImage = document.querySelector(".pokemon__image");
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+const bntPrev = document.querySelector(".btn-prev");
+const bntNext = document.querySelector(".btn-next");
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toString().toLowerCase()}`
+  );
+
+  if (APIResponse.status == 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = "Pesquisando...";
+  pokemonNumber.innerHTML = "";
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = "block";
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id + " -";
+    pokemonImage.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+    input.value = "";
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "Não Encontrado";
+    input.value = "";
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value);
+});
+
+bntPrev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+bntNext.addEventListener("click", () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
